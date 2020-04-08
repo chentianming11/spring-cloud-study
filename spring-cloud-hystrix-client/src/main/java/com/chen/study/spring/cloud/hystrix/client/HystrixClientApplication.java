b07f4d91f72e1724e37e2d55a86270ad5638e1dd
陈添明 <CHENTIANMING002@ke.com>
2020-04-08 21:17:04 +0800

    turbine

--- spring-cloud-hystrix-client/src/main/java/com/chen/study/spring/cloud/hystrix/client/HystrixClientApplication.java
+++ spring-cloud-hystrix-client/src/main/java/com/chen/study/spring/cloud/hystrix/client/HystrixClientApplication.java
@@ -4,6 +4,7 @@ package com.chen.study.spring.cloud.hystrix.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  *
@@ -14,6 +15,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 //@EnableHystrix
 // 激活熔断保护器  hystrix.stream
 @EnableCircuitBreaker
+@EnableTurbine
 public class HystrixClientApplication {
 
     public static void main(String[] args) {
